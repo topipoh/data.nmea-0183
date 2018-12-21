fb39f2bb734a68791041dfaaf5714dff76f4a1dc
Toni Dahl <toni.dahl@solita.fi>
2018-12-21 12:53:02 +0200

    Add support for gprre sentences.

--- src/data/nmea_0183/sentences.clj
+++ src/data/nmea_0183/sentences.clj
@@ -13,17 +13,36 @@
       result
       (if (vector? kw)
         ;; Vector definition of [count field-type-keyword collection-keyword validator] that specifies a collection
-        (let [[cnt kw coll-kw validator] kw
-              new-value (f/parse-field kw value)
-              result (if (or (nil? validator)
-                             (validator new-value))
-                       (update result coll-kw (fnil conj []) new-value)
-                       result)]
-          (if (= 1 cnt)
-            ;; Last one parsed
-            (recur result values keywords)
-            ;; Still more to parse
-            (recur result values (cons [(dec cnt) kw coll-kw validator] keywords))))
+        (let [[cnt coll-kw validator] kw]
+          ;; If coll-key is a map, we take key and value pairs from the processed fields.
+          (if (= (first (s/describe coll-kw)) 'map-of)
+
+            (let [[_ key-kw val-kw] (s/describe coll-kw)
+                  ;; If we are dealing with a dynamic length collection, assume that a collection length is defined
+                  ;; before the collection values.
+                  cnt (or cnt (second (last result)))
+                  new-key (f/parse-field key-kw value)
+                  new-value (f/parse-field val-kw (first values))
+                  result (if (or (nil? validator) (validator new-key))
+                           (update result coll-kw assoc new-key new-value)
+                           result)]
+              (if (= 1 cnt)
+                ;; Last one parsed
+                (recur result (rest values) keywords)
+                ;; Still more to parse
+                (recur result (rest values) (cons [(dec cnt) coll-kw validator] keywords))))
+            ;; If coll-key is a sequence
+            (let [kw (second (s/describe coll-kw))
+                  new-value (f/parse-field kw value)
+                  result (if (or (nil? validator)
+                                 (validator new-value))
+                           (update result coll-kw (fnil conj []) new-value)
+                           result)]
+              (if (= 1 cnt)
+                ;; Last one parsed
+                (recur result values keywords)
+                ;; Still more to parse
+                (recur result values (cons [(dec cnt) coll-kw validator] keywords))))))
 
         ;; Regular single values field
         (recur (assoc result kw (f/parse-field kw value))
@@ -47,7 +66,7 @@
 (define-sentence "GSA" ::gsa
   ::f/faa-mode
   ::f/fix-status
-  [12 ::f/satellite-id ::f/satellite-ids (complement str/blank?)]
+  [12 ::f/satellite-ids (complement str/blank?)]
   ::f/position-dop
   ::f/horizontal-dop
   ::f/vertical-dop)
@@ -75,6 +94,13 @@
 (define-sentence "ROT" ::rot
   ::f/rate-of-turn
   ::f/data-status)
+
+(define-sentence "RRE" ::rre
+  ::f/satellites-in-use
+  ;; Using nil as cnt tells the parser that the collection length is dynamic.
+  [nil ::f/range-residuals (complement str/blank?)]
+  ::f/hpos-err-estimate
+  ::f/vpos-err-estimate)
 
 (define-sentence "ZDA" ::zda
   ::f/time ::f/day ::f/month ::f/year
